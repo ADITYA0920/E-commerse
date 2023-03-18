dd96d3bd66f13d56fc3813b16e97c557a56587c4
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-03-18 11:45:58 +0530

    Added Models

---
+++ src/main/java/com/ChinaMarket/Chinamarket/Model/Seller.java
@@ -0,0 +1,35 @@
+package com.ChinaMarket.Chinamarket.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="seller")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String mobNo;
+
+    @Column(unique = true)
+    private String panNo;
+
+    @OneToMany(mappedBy = "seller",cascade = CascadeType.ALL)
+    List<Product> products = new ArrayList<>();
+}
