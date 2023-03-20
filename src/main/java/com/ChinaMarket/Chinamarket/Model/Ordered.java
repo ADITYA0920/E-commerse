0f60995df07d176e853991d8abc22208b14c752b
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-03-20 23:38:35 +0530

    Added Product and Seller APIs

--- src/main/java/com/ChinaMarket/Chinamarket/Model/Ordered.java
+++ src/main/java/com/ChinaMarket/Chinamarket/Model/Ordered.java
@@ -15,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="orders")
+@Table(name="ordered")
 public class Ordered {
 
     @Id
--- src/main/java/com/ChinaMarket/Chinamarket/Model/Product.java
+++ src/main/java/com/ChinaMarket/Chinamarket/Model/Product.java
@@ -3,6 +3,7 @@ package com.ChinaMarket.Chinamarket.Model;
 import com.ChinaMarket.Chinamarket.Enum.ProductCategory;
 import com.ChinaMarket.Chinamarket.Enum.ProductStatus;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 @Table(name="product")
 public class Product {
 
